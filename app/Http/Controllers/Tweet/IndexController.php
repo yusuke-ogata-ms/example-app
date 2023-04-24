7d3b73d599d64215a0a51b1b6b12be170f2ac501
yusuke ogata <yusuke.ogata.0618@outlook.jp>
2023-04-25 07:01:07 +0900

    画面への表示完了

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -5,13 +5,40 @@ namespace App\Http\Controllers\Tweet;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// option2: Cacades\View を利用して呼び出し
+// use Illuminate\Support\Facades\View;
+
+// option3: Factory をインジェクションして呼び出し
+// use Illuminate\View\Factory;
+
 class IndexController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * html ファイルの呼び出し
+     * resources\views\tweet\index.blade.php を配置する
      */
+    // option1: view ヘルパー関数でhtmlを呼び出し
     public function __invoke(Request $request)
     {
-        return 'Ssingle Action';
+    // option1.1: view 関数の引数で呼び出し
+    //     return view('tweet.index', ['name' => 'laravel']);
+    
+    // option1.2: view はwith関数で引数を渡すことができる
+    // with はメソッドチェーンでいくつも呼び出せる
+         return view('tweet.index')
+            ->with('name', 'withで引数を追加');
     }
+    
+    // option2: Facades\View を利用して呼び出し
+    // public function __invoke(Request $request)
+    // {
+    //     return View::make('tweet.index', ['name' => 'Facades を利用して呼び出し']);
+    // }
+
+    // option3: Factory をインジェクションして呼び出し
+    // public function __invoke(Request $request, Factory $factory)
+    // {
+    //     return $factory->make('tweet.index', ['name' => 'Factory で呼び出し']);
+    // }
+
 }
