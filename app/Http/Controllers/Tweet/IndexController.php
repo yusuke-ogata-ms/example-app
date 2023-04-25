f99c0ac342311a7c526e6341ff6d74ad311c79e2
yusuke ogata <yusuke.ogata.0618@outlook.jp>
2023-04-25 21:02:41 +0900

    seeder の追加。factory によるseed生成。htmlでの表示

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -11,6 +11,9 @@ use Illuminate\Http\Request;
 // option3: Factory をインジェクションして呼び出し
 // use Illuminate\View\Factory;
 
+// seeder で生成したダミーのつぶやきを表示する
+use App\Models\Tweet;
+
 class IndexController extends Controller
 {
     /**
@@ -18,16 +21,16 @@ class IndexController extends Controller
      * resources\views\tweet\index.blade.php を配置する
      */
     // option1: view ヘルパー関数でhtmlを呼び出し
-    public function __invoke(Request $request)
-    {
-    // option1.1: view 関数の引数で呼び出し
-    //     return view('tweet.index', ['name' => 'laravel']);
+    // public function __invoke(Request $request)
+    // {
+    // // option1.1: view 関数の引数で呼び出し
+    // //     return view('tweet.index', ['name' => 'laravel']);
     
-    // option1.2: view はwith関数で引数を渡すことができる
-    // with はメソッドチェーンでいくつも呼び出せる
-         return view('tweet.index')
-            ->with('name', 'withで引数を追加');
-    }
+    // // option1.2: view はwith関数で引数を渡すことができる
+    // // with はメソッドチェーンでいくつも呼び出せる
+    // //      return view('tweet.index')
+    // //         ->with('name', 'withで引数を追加');
+    // }
     
     // option2: Facades\View を利用して呼び出し
     // public function __invoke(Request $request)
@@ -40,5 +43,14 @@ class IndexController extends Controller
     // {
     //     return $factory->make('tweet.index', ['name' => 'Factory で呼び出し']);
     // }
+
+    // seeder で生成したダミーのつぶやきを表示する
+    public function __invoke(Request $request){
+        $tweets = Tweet::all();
+        //dd($tweets);
+        return view('tweet.index')
+            ->with('tweets', $tweets);
+    }
+
 
 }
