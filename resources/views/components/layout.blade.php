3df1c99f85b610bb3912991a81ebdeb562e55d16
yusuke ogata <yusuke.ogata.0618@outlook.jp>
2023-05-02 20:14:17 +0900

    vite + css + js を公式サイトを見て組み込み

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,8 +6,9 @@
     content="width=device-width, user-scalable-no, initial-scale=1.0,
     maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv=X-UA-Compatible" content="ie=edge">
-    <link href="/css/app.css" rel="stylesheet">
-    <script src="/js/app.js" async defer></script>
+    {{-- <link href="/css/app.css" rel="stylesheet">
+    <script src="/js/app.js" async defer></script> --}}
+    @vite([ 'resouces/css/app.css', 'resources/js/app.js' ])
   <title>{{ $title ?? 'つぶやきアプリ' }}</title>
 </head>
 <body class="bg-gray-50">
