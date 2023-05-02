df74988d2e261ed03d4d69ea5a96db280a6cb55f
yusuke ogata <yusuke.ogata.0618@outlook.jp>
2023-05-03 07:36:04 +0900

    ログイン状態の判定による画面遷移、ツイートの一覧表示

--- resources/views/components/tweet/form/post.blade.php
+++ resources/views/components/tweet/form/post.blade.php
@@ -49,3 +49,26 @@
     </form>
   </div>
 @endauth 
+
+@guest 
+<div
+  class="
+    flex
+    flex-wrap
+    justify-center
+  "
+>
+  <div
+    class="
+      w-1/2
+      p-4
+      flex
+      flex-wrap
+      justify-evenly
+    "
+  >
+    <x-element.button-a :href="route('login')">ログイン</x-element.button-a>
+    <x-element.button-a :href="route('register')" theme="secondary">会員登録</x-element.button-a>
+  </div>
+</div>
+@endguest 
