e2f666d627f2409dfb112cd4c1d1a1b54d2c2d15
yusuke ogata <yusuke.ogata.0618@outlook.jp>
2023-05-02 21:34:11 +0900

    app.css がパスのタイポで読み込めていなかったため改修

--- resources/views/components/tweet/form/post.blade.php
+++ resources/views/components/tweet/form/post.blade.php
@@ -7,16 +7,27 @@
         <textarea
           name="tweet" 
           rows="3"
-          class="focus:ring-blue-400
-            focus:border-blue-400 mt-1 block w-full
-            sm:text-sm border border-gray-300 rounded-md p-2"
+          class="
+            focus:ring-blue-400
+            focus:border-blue-400
+            mt-1
+            block
+            w-full
+            sm:text-sm
+            border
+            border-gray-300
+            rounded-md
+            p-2
+            "
           placeholder="つぶやきを入力">
         </textarea>
       </div>
 
-      <p class="mt-2
+      <p class="
+        mt-2
         text-sm
-        text-gray-500">
+        text-gray-500
+        ">
         140文字まで
       </p>
 
@@ -26,9 +37,11 @@
         </x-alert.error>
       @enderror
 
-      <div class="flex
+      <div class="
+        flex
         flex-wrap
-        justify-end">
+        justify-end
+        ">
         <x-element.button>
           つぶやく
         </x-element.button>
