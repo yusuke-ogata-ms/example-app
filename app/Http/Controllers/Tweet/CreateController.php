40ac1e856869c564eaec975f1c98fb505d1f141e
yusuke ogata <yusuke.ogata.0618@outlook.jp>
2023-05-06 08:35:43 +0900

    画像をアップできる

--- app/Http/Controllers/Tweet/CreateController.php
+++ app/Http/Controllers/Tweet/CreateController.php
@@ -8,17 +8,20 @@ use Illuminate\Http\Request;
 use App\Http\Requests\Tweet\CreateRequest;
 use App\Models\Tweet;
 
+use App\Services\TweetService;
+
 class CreateController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(CreateRequest $request)
+    public function __invoke(CreateRequest $request, TweetService $tweetService)
     {
-        $tweet = new Tweet;
-        $tweet->user_id = $request->userId();
-        $tweet->content = $request->tweet();
-        $tweet->save();
+        $tweetService->saveTweet(
+            $request->userId(),
+            $request->tweet(),
+            $request->images()
+        );
 
         return redirect()->route('tweet.index');
     }
